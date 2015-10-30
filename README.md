fb548d84c4654da49b3f3c7ba4e63fb496211873
Frank Robert <frankie.robert@gmail.com>
2015-10-30 19:45:52 -0400

    Music + fixed functions + use strict
    Added music to the body of the HTML. Made everything a part of the
    prototypes and the objects as well as made them all strict.

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
-Slippy's Adventure
+#**Slippy's Adventure**
 
 Slippy is finally setting off on his own and his friends are all here to watch! Take control of slippy's ship and fly him to the energy ring to win!
 
-- Game runs in browser
+- **How-To:**
 
-- Controls:
+- Download .zip of the game
+- Unzip Slippy's Adventure
+- Launch .html file in browser of choice
+- Get to the energy rings!
+
+- **Controls**:
 UP
 DOWN
 LEFT
 RIGHT
 
-- Rules: Advance to the end of the screen and reach an energy ring without touching any of the enemy ships (while impressing all of Slippy's friends!)
+- **Rules**: Advance to the end of the screen and reach an energy ring without touching any of the enemy ships (while impressing all of Slippy's friends!)
 
 When you collide with a ship you lose the game. Both the enemy and your ship explode and you will be reset at the beginning.
--- js/app.js
+++ js/app.js
@@ -1,38 +1,40 @@
 // Enemies our player must avoid
 var Enemy = function() {
+	"use strict";
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = randomShip();
-    this.x = randomX();
-    this.y = randomY();
+    this.x = this.randomX();
+    this.y = this.randomY();
+	this.sprite = this.randomShip();
     this.speed = Math.floor(Math.random() * (600 - 100)) + 100;
 };
 
 // variables and functions to return random X & Y coordinates
 
-var valueX = [-100, -175, -150, -225, -300];
-var valueY = [84, 171, 255];
-
-var randomX = function() {
-    return valueX[Math.floor(Math.random() * 5)];
+Enemy.prototype.randomX = function() {
+	"use strict";
+	var valueX = [-100, -175, -150, -225, -300];
+	return valueX[Math.floor(Math.random() * 5)];
 };
-
-var randomY = function() {
-    return valueY[Math.floor(Math.random() * 3)];
-};
-
+Enemy.prototype.randomY = function() {
+	var valueY = [84, 171, 255];
+	return valueY[Math.floor(Math.random() * 3)];
+	};
 // variable and function to return a random enemy image
-var shipsArray = ['images/ship-2.png', 'images/ship-3.png', 'images/ship-4.png'];
-var randomShip = function() {
-    return shipsArray[Math.floor(Math.random() * 3)];
+
+Enemy.prototype.randomShip = function() {
+	"use strict";
+	var shipsArray = ['images/ship-2.png', 'images/ship-3.png', 'images/ship-4.png'];
+	return shipsArray[Math.floor(Math.random() * 3)];
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+	"use strict";
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -41,19 +43,21 @@ Enemy.prototype.update = function(dt) {
     // when bug goes off screen, bring it back
     // with random coordinates + speed
     if (this.x > 750) {
-        this.x = randomX();
-        this.y = randomY();
+    	this.x = this.randomX();
+		this.y = this.randomY();
         this.speed = Math.floor(Math.random() * (600 - 100)) + 100;
     }
 };
 
 Enemy.prototype.shipReset = function() {
-    this.x = randomX();
-    this.y = randomY();
+	"use strict";
+	this.x = this.randomX();
+	this.y = this.randomY();
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+	"use strict";
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -61,6 +65,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
+	"use strict";
     this.sprite = 'images/ship.png';
     this.playerReset();
 };
@@ -69,6 +74,7 @@ var Player = function() {
 // and compares it to the Player instance's values.
 // calls a reset on the player and the bug when True.
 Player.prototype.collision = function() {
+	"use strict";
     for (var i = 0; i < allEnemies.length; i++) {
         if (this.x < allEnemies[i].x + 50 &&
             this.x + 50 > allEnemies[i].x &&
@@ -83,6 +89,7 @@ Player.prototype.collision = function() {
 
 // boundaries for the player
 Player.prototype.update = function(dt) {
+	"use strict";
     if (this.x < -2) {
         this.x = -2;
     }
@@ -102,11 +109,13 @@ Player.prototype.update = function(dt) {
 
 
 Player.prototype.render = function() {
+	"use strict";
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // move player based on the pixels determined in engine for col / row
 Player.prototype.handleInput = function(keys) {
+	"use strict";
     switch (keys) {
         case "up":
             this.y -= 83;
@@ -126,6 +135,7 @@ Player.prototype.handleInput = function(keys) {
 };
 // instantiate the player
 Player.prototype.playerReset = function() {
+	"use strict";
     this.x = 202;
     this.y = 420;
 };
@@ -140,6 +150,7 @@ var player = new Player();
 
 // function to create new ships / enemies
 var createShips = function() {
+	"use strict";
     var ship = 4;
     var count = 0;
     while (ship > count) {
@@ -149,9 +160,11 @@ var createShips = function() {
     }
 };
 createShips();
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
+	"use strict";
     var allowedKeys = {
         37: 'left',
         38: 'up',
